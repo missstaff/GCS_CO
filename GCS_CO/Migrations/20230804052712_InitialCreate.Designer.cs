4b4cd03c1edd0455316264b61c32ef3cbf276d6d
missstaff <shawnastaff@gmail.com>
2023-08-03 22:31:19 -0700

    good place to commit before adding a custom app user. The database is seeded w/ 1 admin user, 1 manager use, and 1 team member user. Role generation works! Again data is seeded. This is sufficient for this project but I do want an "AppUser" model to allow for an account dashboard.

--- GCS_CO/Migrations/20230804052712_InitialCreate.Designer.cs
+++ GCS_CO/Migrations/20230804052712_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GCS_CO.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230804045957_InitialCreate")]
+    [Migration("20230804052712_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
