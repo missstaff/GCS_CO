a3abd2d5dcd1aac5e8e6573554188e9826bd7040
missstaff <shawnastaff@gmail.com>
2023-08-08 19:58:26 -0700

    An admin can view list of appusers, can view an app user's details, can delete an app user

--- GCS_CO/Controllers/Admin/AppUsersController.cs
+++ GCS_CO/Controllers/Admin/AppUsersController.cs
@@ -2,7 +2,9 @@
 using GCS_CO.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace GCS_CO.Controllers.Admin
 {
@@ -22,7 +24,6 @@ namespace GCS_CO.Controllers.Admin
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: AppUsersController/Details/5
         public async Task<IActionResult> Details(string? id)
         {
             if (id == null || _context.Users == null)
@@ -39,5 +40,70 @@ namespace GCS_CO.Controllers.Admin
             ViewBag.UserRoles = userRoles; // Pass the roles to the view
             return View(user);
         }
+
+
+        public IActionResult Create()
+        {
+            var roles = _context.Roles.Select(r => r.Name).ToList();
+            ViewBag.UserRoles = new SelectList(roles);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,PhoneNumber,DateHired,RegionId,SkillId,AddressId")] AppUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+          
+            return View(user);
+        }
+      
+        public async Task<IActionResult> Delete(string? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            ViewBag.UserRoles = userRoles;
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Employees'  is null.");
+            }
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppUserExists(string id)
+        {
+            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
+
