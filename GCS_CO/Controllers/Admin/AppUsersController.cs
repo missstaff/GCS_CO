374352e06e08a3389cbdfda5c8fa58f5fee4d388
missstaff <shawnastaff@gmail.com>
2023-08-08 18:50:25 -0700

    removed unused using statements

--- GCS_CO/Controllers/Admin/AppUsersController.cs
+++ GCS_CO/Controllers/Admin/AppUsersController.cs
@@ -1,6 +1,5 @@
 ﻿using GCS_CO.Data;
 using GCS_CO.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
--- GCS_CO/Controllers/Admin/EmployeesController.cs
+++ GCS_CO/Controllers/Admin/EmployeesController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GCS_CO.Data;
 using GCS_CO.Models;
 using Microsoft.AspNetCore.Authorization;
-using System.Data;
 
 namespace GCS_CO.Controllers.Admin
 {
