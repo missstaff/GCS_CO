c42f219da617c7e511fc267fcdcf99f45b399569
missstaff <shawnastaff@gmail.com>
2023-08-08 20:22:18 -0700

    an admin can edit an appuser and their role

--- GCS_CO/Controllers/Admin/AppUsersController.cs
+++ GCS_CO/Controllers/Admin/AppUsersController.cs
@@ -62,7 +62,73 @@ namespace GCS_CO.Controllers.Admin
           
             return View(user);
         }
-      
+
+        public async Task<IActionResult> Edit(string? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var roles = _context.Roles.Select(r => r.Name).ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var selectListRoles = new SelectList(roles, userRoles.FirstOrDefault()); // Set the selected role
+            ViewBag.UserRoles = selectListRoles;
+            return View(user);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,FirstName,LastName,Email,PhoneNumber")] AppUser updatedUser, string selectedRole)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users.FindAsync(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                user.FirstName = updatedUser.FirstName;
+                user.LastName = updatedUser.LastName;
+                user.Email = updatedUser.Email;
+                user.PhoneNumber = updatedUser.PhoneNumber;
+
+                var currentRoles = await _userManager.GetRolesAsync(user);
+                await _userManager.RemoveFromRolesAsync(user, currentRoles);
+                await _userManager.AddToRoleAsync(user, selectedRole);
+
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppUserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(updatedUser);
+        }
+
+
         public async Task<IActionResult> Delete(string? id)
         {
             if (id == null || _context.Users == null)
